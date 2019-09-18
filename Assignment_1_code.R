063287c954fd17914f63eae7abcd5e6318a40aa9
lisa9000 <lisasalomons9000@gmail.com>
2019-09-18 15:55:01 +0200

    second commit

--- Assignment_1_code.R
+++ Assignment_1_code.R
@@ -31,31 +31,22 @@ best_split <- function(x, y) {
   parent_i <- impurity.gini(y)
   x.sorted <- sort(unique(x))
   x.length <- length(x.sorted)
-  # Acount for the binary values for faster computation
-  if (x.length == 2){
-    best_split_point <- 0.5
-    big_split <- y[x > 0.5]
-    small_split <- y[x <= 0.5]
-    best_reduction <- parent_i -(impurity.gini(small_split) * (length(small_split)/length(x))+ impurity.gini(big_split)*(length(big_split)/length(x)))
-    best_split_rows <- get_x_index(x, best_split_point)
-  } else {
-    x.splitpoints <- x.sorted[2]
-    x.splitpoints <- (x.sorted[1:x.length-1]+x.sorted[2:x.length])/2
-    best_split_point <- 0
-    best_reduction <- 0
-    # Loop over the data to determine the best split point based on impurity reduction
-    for (val in x.splitpoints) {
-      big_split <- y[x > val]
-      small_split <- y[x <= val]
-      big_imp <- impurity.gini(big_split)
-      small_imp <- impurity.gini(small_split)
-      reduction_imp = parent_i -(small_imp * (length(small_split)/length(x))+big_imp*(length(big_split)/length(x)))
-      if (reduction_imp > best_reduction) {
-        best_split_point <- val
-        best_reduction <- reduction_imp
-        best_split_rows = get_x_index(x, best_split_point)
-      }
-    }  
+  x.splitpoints <- x.sorted[2]
+  x.splitpoints <- (x.sorted[1:x.length-1]+x.sorted[2:x.length])/2
+  best_split_point <- 0
+  best_reduction <- 0
+  # Loop over the data to determine the best split point based on impurity reduction
+  for (val in x.splitpoints) {
+    big_split <- y[x > val]
+    small_split <- y[x <= val]
+    big_imp <- impurity.gini(big_split)
+    small_imp <- impurity.gini(small_split)
+    reduction_imp = parent_i -(small_imp * (length(small_split)/length(x))+big_imp*(length(big_split)/length(x)))
+    if (reduction_imp > best_reduction) {
+      best_split_point <- val
+      best_reduction <- reduction_imp
+      best_split_rows = get_x_index(x, best_split_point)
+     }
   }
   return(list(best_reduction, best_split_point, unlist(best_split_rows[1]), unlist(best_split_rows[2])))
 }
@@ -75,19 +66,43 @@ get_split <- function(x, y, nfeat) {
   return(b_split)
 }
 
-tree.grow <- function(x, y, nmin, minleaf, nfeat){
-  store <- Dict$new(items = NULL)
-  root_split <- get_split(x, y, nfeat)
-  # Get the attribute names
-  attributes <- colnames(x)
-  root_name <- attributes[unlist(root_split[1])]
-  tree <- Node$new(root_name, split_point = root_split[2], data_l = root_split[3], data_r = root_split[4])
-  tree$AddChild(root_name, split_point = root_split[2], data_l = root_split[3], data_r = root_split[4])
-  print(tree, "split_point", "data_l", "data_r")
-  plot(tree)
+toLeafNode <- function(tree, node, y) {
+  data <- c(node$Get("data_l"), node$Get("data_r"))
 }
 
-credit <- read.csv('C:/Users/Lisa/Desktop/UU/Data_Mining/Assignment_1/credit.txt', header = TRUE)
+split <- function(node, x, y, nmin, minleaf, nfeat, attributes) {
+  l <- node$Get('data_l')
+  r <- node$Get('data_r')
+  
+  if (length(left) < nmin){
+    toLeafNode(tree, node, y)
+  } else {
+    split_l <- get_split(x, y, nfeat)
+    print(split_l)
+    node$AddChild('left', attribute_n = attributes[unlist(split_l[1])], split_point = split_l[2], data_l = split_l[3], data_r = split_l[4])
+  } 
+  if (length(right) < nmin) {
+    toLeafNode(tree, node, y)
+  } else {
+    split_l <- get_split(x, y, nfeat)
+    node$AddChild('left', attribute_n = attributes[unlist(split_l[1])], split_point = split_l[2], data_l = split_l[3], data_r = split_l[4])
+  }
+  # print(node, 'attribute_n','splitpoint', 'data_l', 'data_r')
+}
+
+
+tree.grow <- function(x, y, nmin, minleaf, nfeat){
+  # Get the attribute names
+  attributes <- colnames(x)
+  root_split <- get_split(x, y, nfeat)
+  root_name <- attributes[unlist(root_split[1])]
+  # print(root_split[2])
+  tree <- Node$new("start", attribute_n = root_name, split_point = root_split[2], data_l = root_split[3], data_r = root_split[4])
+  # print(tree, 'attribute_n','splitpoint', 'data_l', 'data_r')
+  split(tree, x, y, nmin, minleaf, nfeat, attributes)
+}
+
+credit <- read.csv('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/credit.txt', header = TRUE)
 # print(credit)
 b_split <- best_split(credit[,4],credit[,6])
 cx <- credit[,4]
