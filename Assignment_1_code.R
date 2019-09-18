eb645fbc8dfd21189d2b48ccb84ae1c94117e322
lisa9000 <lisasalomons9000@gmail.com>
2019-09-18 18:13:34 +0200

    tree.grow works on small dataset

--- Assignment_1_code.R
+++ Assignment_1_code.R
@@ -27,15 +27,14 @@ get_x_index <- function(x, split_point){
 }
 
 # Returns the best split for an attribute of the dataset
-best_split <- function(x, y, minleaf) {
-  
+best_split <- function(x, x_row, y, minleaf) {
   parent_i <- impurity.gini(y)
   x.sorted <- sort(unique(x))
   x.length <- length(x.sorted)
   x.splitpoints <- x.sorted[2]
   x.splitpoints <- (x.sorted[1:x.length-1]+x.sorted[2:x.length])/2
-  best_split_point <- 0
-  best_reduction <- 0
+  best_split_point <- -999
+  best_reduction <- -999
   # Loop over the data to determine the best split point based on impurity reduction
   for (val in x.splitpoints) {
     big_split <- y[x > val]
@@ -43,13 +42,17 @@ best_split <- function(x, y, minleaf) {
     big_imp <- impurity.gini(big_split)
     small_imp <- impurity.gini(small_split)
     reduction_imp = parent_i -(small_imp * (length(small_split)/length(x))+big_imp*(length(big_split)/length(x)))
+    
     if (length(small_split) >= minleaf && length(big_split) >= minleaf) {
       if (reduction_imp > best_reduction) {
         best_split_point <- val
         best_reduction <- reduction_imp
-        best_split_rows = get_x_index(x, best_split_point)
+        best_split_rows <- list(x_row[x > val], x_row[x <= val])
       }
-    } 
+    }
+  }
+  if (best_split_point == -999) {
+    return(NULL)
   }
   return(list(best_reduction, best_split_point, unlist(best_split_rows[1]), unlist(best_split_rows[2])))
 }
@@ -59,7 +62,9 @@ get_split <- function(x_row, y, nfeat, minleaf) {
   x <- credit[x_row, ]
   b_gini <- 0
   for (i in 1:nfeat){
-    split <- best_split(x[,i], y, minleaf)
+    split <- best_split(x[,i], x_row, y, minleaf)
+    
+    if (is.null(split)) next
     gini <- unlist(split[1])
     # The best split is the split with best reduction
     if (gini > b_gini){
@@ -71,6 +76,7 @@ get_split <- function(x_row, y, nfeat, minleaf) {
 }
 
 tree.grow <- function(x, y, nmin, minleaf, nfeat){
+  attribute_names = colnames(x)
   root <- Node$new('start', data_x = c(1:length(x[,1])), data_y = y)
   nodelist <- list(root)
   while(length(nodelist) > 0) {
@@ -89,13 +95,13 @@ tree.grow <- function(x, y, nmin, minleaf, nfeat){
       left_y <- y[left_data]
       right_y <- y[right_data]
 
-      leftchild <- node$AddChild('left', data_x = left_data, data_y = left_y, split_attribute = split_attribute, split_value = split_value)
-      rightchild <- node$AddChild('right', data_x = right_data, data_y = right_y, split_attribute = split_attribute, split_value = split_value)
+      leftchild <- node$AddChild('Bigger', data_x = left_data, data_y = left_y, split_attribute = attribute_names[split_attribute], split_value = split_value)
+      rightchild <- node$AddChild('Smaller', data_x = right_data, data_y = right_y, split_attribute = attribute_names[split_attribute], split_value = split_value)
     
       nodelist <- c(nodelist, list(leftchild, rightchild))
     }
   }
-  
+  print(root, 'split_attribute', "split_value", "data_y")
 }
 
 credit <- read.csv('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/credit.txt', header = TRUE)
