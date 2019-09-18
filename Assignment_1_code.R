a3f79c47429f9fc6a5843ed27a55c298216a67fa
lisa9000 <lisasalomons9000@gmail.com>
2019-09-18 16:39:20 +0200

    thirt commit

--- Assignment_1_code.R
+++ Assignment_1_code.R
@@ -27,7 +27,7 @@ get_x_index <- function(x, split_point){
 }
 
 # Returns the best split for an attribute of the dataset
-best_split <- function(x, y) {
+best_split <- function(x, y, minleaf) {
   parent_i <- impurity.gini(y)
   x.sorted <- sort(unique(x))
   x.length <- length(x.sorted)
@@ -42,20 +42,22 @@ best_split <- function(x, y) {
     big_imp <- impurity.gini(big_split)
     small_imp <- impurity.gini(small_split)
     reduction_imp = parent_i -(small_imp * (length(small_split)/length(x))+big_imp*(length(big_split)/length(x)))
-    if (reduction_imp > best_reduction) {
-      best_split_point <- val
-      best_reduction <- reduction_imp
-      best_split_rows = get_x_index(x, best_split_point)
-     }
+    if (length(small_split) >= minleaf && length(big_split) >= minleaf) {
+      if (reduction_imp > best_reduction) {
+        best_split_point <- val
+        best_reduction <- reduction_imp
+        best_split_rows = get_x_index(x, best_split_point)
+      }
+    } 
   }
   return(list(best_reduction, best_split_point, unlist(best_split_rows[1]), unlist(best_split_rows[2])))
 }
 
 # Determine the best split over all of the attributes at a certain node
-get_split <- function(x, y, nfeat) {
+get_split <- function(x, y, nfeat, minleaf) {
   b_gini <- 0
   for (i in 1:nfeat){
-    split <- best_split(x[,i], y)
+    split <- best_split(x[,i], y, minleaf)
     gini <- unlist(split[1])
     # The best split is the split with best reduction
     if (gini > b_gini){
@@ -66,51 +68,33 @@ get_split <- function(x, y, nfeat) {
   return(b_split)
 }
 
-toLeafNode <- function(tree, node, y) {
-  data <- c(node$Get("data_l"), node$Get("data_r"))
-}
-
-split <- function(node, x, y, nmin, minleaf, nfeat, attributes) {
-  l <- node$Get('data_l')
-  r <- node$Get('data_r')
-  
-  if (length(left) < nmin){
-    toLeafNode(tree, node, y)
-  } else {
-    split_l <- get_split(x, y, nfeat)
-    print(split_l)
-    node$AddChild('left', attribute_n = attributes[unlist(split_l[1])], split_point = split_l[2], data_l = split_l[3], data_r = split_l[4])
-  } 
-  if (length(right) < nmin) {
-    toLeafNode(tree, node, y)
-  } else {
-    split_l <- get_split(x, y, nfeat)
-    node$AddChild('left', attribute_n = attributes[unlist(split_l[1])], split_point = split_l[2], data_l = split_l[3], data_r = split_l[4])
-  }
-  # print(node, 'attribute_n','splitpoint', 'data_l', 'data_r')
-}
-
-
 tree.grow <- function(x, y, nmin, minleaf, nfeat){
-  # Get the attribute names
-  attributes <- colnames(x)
-  root_split <- get_split(x, y, nfeat)
-  root_name <- attributes[unlist(root_split[1])]
-  # print(root_split[2])
-  tree <- Node$new("start", attribute_n = root_name, split_point = root_split[2], data_l = root_split[3], data_r = root_split[4])
-  # print(tree, 'attribute_n','splitpoint', 'data_l', 'data_r')
-  split(tree, x, y, nmin, minleaf, nfeat, attributes)
+  root <- Node$new('start', data_x = x, data_y = y)
+  nodelist <- list(root)
+  while(length(nodelist) > 0) {
+    node <- nodelist[[1]]
+    nodelist[[1]] <- NULL
+    
+    if (impurity.gini(node$data_y) > 0 && length(node$data_y) >= nmin) {
+      split <- get_split(node$data_x, node$data_y, minleaf)
+      split_attribute <- split[1]
+      split_value <- split[2]
+      print(split_attribute)
+      print(split_value)
+    }
+  }
+  
 }
 
 credit <- read.csv('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/credit.txt', header = TRUE)
 # print(credit)
-b_split <- best_split(credit[,4],credit[,6])
+# b_split <- best_split(credit[,4],credit[,6])
 cx <- credit[,4]
 
 credit.y <- credit[,6]
 credit.x <- credit[1:5]
 credit.nfeat <- length(credit.x[1,])
-get_split(credit.x, credit.y, credit.nfeat)
+
 
 tree.grow(credit.x, credit.y, 2, 1, credit.nfeat)
 
