9f209d4cf92ef25629304c6f2bfe8558851bee6e
lisa9000 <lisasalomons9000@gmail.com>
2019-09-23 15:08:22 +0200

    random forest works on the small data set

--- Assignment_1_code.R
+++ Assignment_1_code.R
@@ -1,4 +1,3 @@
-library(collections)
 library(data.tree)
 
 # Calculate the impurity of a node using the Gini index
@@ -8,43 +7,47 @@ impurity.gini <- function(values) {
   imp = pos*neg
 }
 
+mode <- function(x) {
+  x_unique <- unique(x)
+  return(x_unique[which.max(tabulate(match(x, x_unique)))])
+}
 
 # Returns the best split for an attribute of the dataset
-best_split <- function(x, x_row, y, minleaf) {
+best.split <- function(x, x_row, y, minleaf) {
   parent_i <- impurity.gini(y)
-  x.sorted <- sort(unique(x))
-  x.length <- length(x.sorted)
-  x.splitpoints <- (x.sorted[1:x.length-1]+x.sorted[2:x.length])/2
-  best_split_point <- -999
+  x_sorted <- sort(unique(x))
+  x_length <- length(x_sorted)
+  x_splitpoints <- (x_sorted[1:x_length-1]+x_sorted[2:x_length])/2
+  best_splitpoint <- -999
   best_reduction <- -999
   # Loop over the data to determine the best split point based on impurity reduction
-  for (val in x.splitpoints) {
-    big_split <- y[x > val]
-    small_split <- y[x <= val]
-    big_imp <- impurity.gini(big_split)
-    small_imp <- impurity.gini(small_split)
-    reduction_imp = parent_i -(small_imp * (length(small_split)/length(x))+big_imp*(length(big_split)/length(x)))
+  for (val in x_splitpoints) {
+    left_split <- y[x > val]
+    right_split <- y[x <= val]
+    left_imp <- impurity.gini(left_split)
+    right_imp <- impurity.gini(right_split)
+    reduction_imp = parent_i -(right_imp * (length(right_split)/length(x))+left_imp*(length(left_split)/length(x)))
     
-    if (length(small_split) >= minleaf && length(big_split) >= minleaf) {
+    if (length(right_split) >= minleaf && length(left_split) >= minleaf) {
       if (reduction_imp > best_reduction) {
-        best_split_point <- val
+        best_splitpoint <- val
         best_reduction <- reduction_imp
-        best_split_rows <- list(x_row[x > val], x_row[x <= val])
+        best_splitrows <- list(x_row[x > val], x_row[x <= val])
       }
     }
   }
-  if (best_split_point == -999) {
+  if (best_splitpoint == -999) {
     return(NULL)
   }
-  return(list(best_reduction, best_split_point, unlist(best_split_rows[1]), unlist(best_split_rows[2])))
+  return(append(c(best_reduction, best_splitpoint),best_splitrows))
 }
 
 # Determine the best split over all of the attributes at a certain node
-get_split <- function(x_row, y, nfeat, minleaf) {
+get.split <- function(x_row, y, nfeat, minleaf) {
   x <- credit[x_row, ]
   b_gini <- 0
   for (i in 1:nfeat){
-    split <- best_split(x[,i], x_row, y, minleaf)
+    split <- best.split(x[,i], x_row, y, minleaf)
     
     if (is.null(split)) next
     gini <- unlist(split[1])
@@ -57,21 +60,20 @@ get_split <- function(x_row, y, nfeat, minleaf) {
   return(b_split)
 }
 
-toLeafNode <- function(node) {
-  max_index <- which.max(node$data_y)
-  node$Set(label = node$data_y[max_index])
+to.leaf <- function(node) {
+  node$Set(label = mode(node$dataY))
 }
 
 tree.grow <- function(x, y, nmin, minleaf, nfeat){
   attribute_names = colnames(x)
-  root <- Node$new('start', data_x = c(1:length(x[,1])), data_y = y)
+  root <- Node$new('start', dataX = c(1:length(x[,1])), dataY = y)
   nodelist <- list(root)
   while(length(nodelist) > 0) {
     node <- nodelist[[1]]
     nodelist[[1]] <- NULL
     
-    if (impurity.gini(node$data_y) > 0 && length(node$data_y) >= nmin) {
-      split <- get_split(node$data_x, node$data_y, nfeat, minleaf)
+    if (impurity.gini(node$dataY) > 0 && length(node$dataY) >= nmin) {
+      split <- get.split(node$dataX, node$dataY, nfeat, minleaf)
       
       split_attribute <- split[[1]]
       split_value <- split[[2]]
@@ -82,16 +84,16 @@ tree.grow <- function(x, y, nmin, minleaf, nfeat){
       left_y <- y[left_data]
       right_y <- y[right_data]
       
-      node$Set(split_attribute = attribute_names[split_attribute], split_value = split_value)
-      leftchild <- node$AddChild('Bigger', data_x = left_data, data_y = left_y)
-      rightchild <- node$AddChild('Smaller', data_x = right_data, data_y = right_y)
+      node$Set(splitAttribute = attribute_names[split_attribute], splitValue = split_value)
+      leftchild <- node$AddChild('leftChild', dataX = left_data, dataY = left_y)
+      rightchild <- node$AddChild('rightChild', dataX = right_data, dataY = right_y)
     
       nodelist <- c(nodelist, list(leftchild, rightchild))
     } else {
-      toLeafNode(node)
+      to.leaf(node)
     }
   }
-  print(root, 'split_attribute', "split_value", "data_y", "label")
+  # print(root, 'splitAttribute', "splitValue", "dataY", "label")
   return(root)
 }
 
@@ -101,26 +103,51 @@ tree.classify <- function(x, tr) {
     node <- tr
     
     while(!isLeaf(node)) {
-      if (x[row, node$split_attribute] > node$split_value) {
-        node <- node$Bigger
+      if (x[row, node$splitAttribute] > node$splitValue) {
+        node <- node$leftChild
       } else {
-        node <- node$Smaller
+        node <- node$rightChild
       }
     }
     y[row] <- node$label
   }
   return(y)
 }
+
+tree.grow.bag <- function(x, y, nmin, minleaf, nfeat, m) {
+  trees <- c()
+  for (i in 1:m) {
+    samples <- sample(nrow(x), nrow(x), TRUE)
+    
+    trees <- append(trees, tree.grow(x[samples, ], y[samples], nmin, minleaf, nfeat))
+  }
+  return(trees)
+}
+
+tree.classify.bag <- function(x, tr) {
+  predictions <- c()
+  for (i in 1:nrow(x)) {
+    labels <- c()
+    for (tree in tr) {
+      labels <- append(labels, tree.classify(x, tree))
+    }
+    predictions[i] <- mode(labels)
+  }
+  return(predictions)
+}
+
+
 credit <- read.csv('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/credit.txt', header = TRUE)
 # # print(credit)
-# # b_split <- best_split(credit[,4],credit[,6])
+# # b_split <- best.split(credit[,4],credit[,6])
 # cx <- credit[,4]
 
-credit.y <- credit[,6]
-credit.x_1 <- credit[1:8,1:5]
-credit.x_2 <- credit[9:10, 1:5]
+credit.y <- credit[1:10,6]
+credit.x_1 <- credit[1:10,1:5]
+credit.x_2 <- credit[11:12, 1:5]
 credit.nfeat <- length(credit.x_1[1,])
 
 tree <- tree.grow(credit.x_1, credit.y, 2, 1, credit.nfeat)
 tree.classify(credit.x_2, tree)
-
+trees <- tree.grow.bag(credit.x_1, credit.y, 2, 1, credit.nfeat, 5)
+tree.classify.bag(credit.x_2, trees)
