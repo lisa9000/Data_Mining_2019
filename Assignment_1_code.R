3c8c19445b15f3e81c6e8386dafe9e5510afd862
lisa9000 <lisasalomons9000@gmail.com>
2019-09-25 18:44:33 +0200

    everything is working

--- Assignment_1_code.R
+++ Assignment_1_code.R
@@ -15,7 +15,7 @@ mode <- function(x) {
 # Returns the best split for an attribute of the dataset
 best.split <- function(x, x_row, y, minleaf) {
   parent_i <- impurity.gini(y)
-  x <- as.numeric(x)
+  x <- as.numeric(as.character(x))
   x_unique <- unique(x)
   x_sorted <- sort(x_unique)
   x_length <- length(x_sorted)
@@ -48,6 +48,7 @@ best.split <- function(x, x_row, y, minleaf) {
 
 # Determine the best split over all of the attributes at a certain node
 get.split <- function(x_row, y, nfeat, minleaf) {
+  # x <- pima_data[x_row, ]
   x <- data_eclipse_2.0[x_row, ]
   b_gini <- 0
   predictors <- sample(ncol(x), nfeat)
@@ -107,7 +108,7 @@ tree.classify <- function(x, tr) {
     node <- tr
     
     while(!isLeaf(node)) {
-      if (as.numeric(x[row, node$splitAttribute]) > node$splitValue) {
+      if (as.numeric(as.character(x[row, node$splitAttribute])) > node$splitValue) {
         node <- node$leftChild
       } else {
         node <- node$rightChild
@@ -132,12 +133,31 @@ tree.classify.bag <- function(x, tr) {
   for (i in 1:nrow(x)) {
     labels <- c()
     for (tree in tr) {
-      labels <- append(labels, tree.classify(x, tree))
+      labels <- append(labels, tree.classify(x[i,], tree))
     }
     predictions[i] <- mode(labels)
   }
   return(predictions)
 }
+
+measures <- function(y, predictions) {
+  TP <- 0
+  FP <- 0
+  TN <- 0 
+  FN <- 0
+  for (i in 1:length(predictions)){
+    if (predictions[i] == 1 && y[i] == 1) TP <- TP + 1
+    if (predictions[i] == 1 && y[i] == 0) FP <- FP + 1
+    if (predictions[i] == 0 && y[i] == 0) TN <- TN + 1
+    if (predictions[i] == 0 && y[i] == 1) FN <- FN + 1
+  }
+  precision <- TP/(TP+FP)
+  recall <- TP/(TP+FN)
+  accuracy <- (TP+TN)/length(predictions)
+  print(c(TP, FP, TN, FN))
+  return(c(precision, recall, accuracy))
+}
+
 
 # training data
 eclipse_2.0 <- read.csv2('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/eclipse-metrics-packages-2.0.csv')
@@ -148,16 +168,23 @@ eclipse_2.0_labels <- as.numeric(eclipse_2.0[,4] > 0)
 eclipse_3.0 <- read.csv2('C:/Users/Lisa/Desktop/UU/Data_Mining_2019/eclipse-metrics-packages-3.0.csv')
 data_eclipse_3.0 <- eclipse_3.0[,c(3, 5:44)]
 eclipse_3.0_labels <- as.numeric(eclipse_3.0[,4] > 0)
-main <- function() {
-  tree_1 <- tree.grow(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 41)
-  predictions_1 <- tree.classify(data_eclipse_3.0, tree_1)
-  
-  trees_1 <- tree.grow.bag(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 41, 100)
-  predictions_2 <- tree.classify.bag(data_eclipse_3.0, trees_1)
-  print(predictions_2)
-  
-  trees_2 <- tree.grow.bag(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 6, 100)
-  predictions_3 <- tree.classify.bag(data_eclipse_3.0, trees_2)
-}
-main()
 
+tree_1 <- tree.grow(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 41)
+predictions_1 <- tree.classify(data_eclipse_3.0, tree_1)
+measures_1 <- measures(eclipse_3.0_labels, predictions_1)
+
+
+trees_1 <- tree.grow.bag(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 41, 100)
+predictions_2 <- tree.classify.bag(data_eclipse_3.0, trees_1)
+measures_2 <- measures(eclipse_3.0_labels, predictions_2)
+print(measures_2)
+
+trees_2 <- tree.grow.bag(data_eclipse_2.0, eclipse_2.0_labels, 15, 5, 6, 100)
+predictions_3 <- tree.classify.bag(data_eclipse_3.0, trees_2)
+measures_3 <- measures(eclipse_3.0_labels, predictions_3)
+
+print(measures(eclipse_3.0_labels, predictions_1))
+head(data_eclipse_2.0)
+summary(as.numeric(as.character(data_eclipse_2.0[,ncol(data_eclipse_2.0)])))
+
+        
