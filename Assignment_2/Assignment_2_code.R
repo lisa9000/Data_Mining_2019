48fd3858fc74227cf24ce014287091e4d6ab16a9
lisa9000 <lisasalomons9000@gmail.com>
2019-10-25 16:46:33 +0200

    Update

--- Assignment_2/Assignment_2_code.R
+++ Assignment_2/Assignment_2_code.R
@@ -7,7 +7,6 @@ library(textmineR)
 library(RWeka)
 
 
-
 loadData <- function(location_t, location_d, from, to){
   data <- matrix(nrow = 1, ncol= 2)
   for(i in from:to){
@@ -35,6 +34,10 @@ loadData <- function(location_t, location_d, from, to){
 
 BiGramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=2,max=2))
 
+convert_values <- function(x){
+  x <- ifelse(x>0, "yes", "no")
+  return(x)
+}
 # Load the train and test set
 train.data <- loadData('./op_spam_v1.4/negative_polarity/truthful_from_Web', './op_spam_v1.4/negative_polarity/deceptive_from_MTurk', 1, 4)
 test.data <-loadData('./op_spam_v1.4/negative_polarity/truthful_from_Web', './op_spam_v1.4/negative_polarity/deceptive_from_MTurk', 5, 5)
@@ -59,32 +62,44 @@ train_corpus <- tm_map(train_corpus, BiGramTokenizer)
 train_corpus <- tm_map(train_corpus, PlainTextDocument)
 train_corpus[[1]]$content
 
+# For unigram
+train_dtm_uni <- DocumentTermMatrix(train_corpus, control = list(tolower = TRUE, stopwords = TRUE, removePunctuation = TRUE,
+                                                             removeNumbers = TRUE, stripWhitespace = TRUE, lemmatize_words = TRUE, weighting = weightTf))
 
-# train_dtm <- DocumentTermMatrix(train_corpus[1], control = list(tolower = TRUE, stopwords = TRUE, removePunctuation = TRUE,
-                                                             # removeNumbers = TRUE, stripWhitespace = TRUE, lemmatize_words = TRUE, tokenize = function(x) NGramTokenizer(x, Weka_control(min=2,max=2))))
-train_dtm <- TermDocumentMatrix(train_corpus[1], control = list(tolower = TRUE, stripWhitespace = TRUE, stopwords = TRUE,removeNumbers = TRUE, tokenize = BiGramTokenizer))
-train_dtm <- DocumentTermMatrix(train_corpus)
-train_dtm$dimnames$Terms[10:100]
-dim(train_dtm)
+# For bigram
+train_dtm_bi <- DocumentTermMatrix(train_corpus, control = list(tolower = TRUE, stripWhitespace = TRUE, stopwords = TRUE, removeNumbers = TRUE, lemmatize_words = TRUE, tokenize = BiGramTokenizer))
+
+train_dtm_uni$dimnames$Terms[1:10]
+dim(train_dtm_bi)
+
 test_corpus <- VCorpus(VectorSource(test.data$text))
-test_dtm <- DocumentTermMatrix(test_corpus,)
 
+# Create a test set for the unigram and bigram
+test_dtm_uni <- DocumentTermMatrix(test_corpus,control = list(tolower = TRUE, stopwords = TRUE, removePunctuation = TRUE,
+                                                          removeNumbers = TRUE, stripWhitespace = TRUE, lemmatize_words = TRUE))
+test_dtm_bi <- DocumentTermMatrix(test_corpus,control = list(tolower = TRUE, stopwords = TRUE, removePunctuation = TRUE,
+                                                          removeNumbers = TRUE, stripWhitespace = TRUE, lemmatize_words = TRUE, tokenize= BiGramTokenizer))
 
-dim(train_dtm)
-dim(test_dtm)
-train_dtm_freq <- removeSparseTerms(train_dtm, 0.95)
-dim(train_dtm_freq)
+# Remove sparse terms
+train_dtm_freq_uni <- removeSparseTerms(train_dtm_uni, 0.975)
+train_dtm_freq_bi <- removeSparseTerms(train_dtm_bi, 0.99)
 
-convert_values <- function(x){
-  x <- ifelse(x>0, "yes", "no")
-  return(x)
-}
+# Create train and test unigram
+train <- apply(train_dtm_freq_uni, MARGIN = 2, convert_values)
+test <- apply(test_dtm_uni, MARGIN = 2, convert_values)
 
-train <- apply(train_dtm_freq, MARGIN = 2, convert_values)
-test <- apply(test_dtm, MARGIN = 2, convert_values)
+# Create train and test categorical Biram
+train_bi <- apply(train_dtm_freq_bi, MARGIN = 2, convert_values)
+test_bi <- apply(test_dtm_bi, MARGIN = 2, convert_values)
 
-NV_classifier <- naiveBayes(train, train.data$label)
+# Naive Bayes classiffier unigram
+NV_classifier <- naiveBayes(train, as.factor(train.data[,2]))
 NV_pred <- predict(NV_classifier, test)
 
+# Naive bayes classifier Bigram
+NV_classifier_bi <- naiveBayes(train_bi, as.factor(train.data[,2]))
+NV_pred_bi <- predict(NV_classifier_bi, test_bi)
 
-confusionMatrix(data = NV_pred, reference = test.data$label, positive = "1",dnn = c("prediction", "actual"))
+# ConfusionMatrices
+confusionMatrix(data = NV_pred, reference = as.factor(test.data[,2]), positive = "1",dnn = c("prediction", "actual"))
+confusionMatrix(data = NV_pred_bi, reference = as.factor(test.data[,2]), positive = "1",dnn = c("prediction", "actual"))
